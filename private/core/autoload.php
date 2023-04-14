4e97f063ece28b86114b7604abb61d0705145f09
jatin-08 <yadavjatin137@gmail.com>
2023-04-14 19:33:08 +0530

    Done autoload

---
+++ private/core/autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("app.php");
+require_once("config.php");
+require_once("controller.php");
+require_once("database.php");
+require_once("function.php");
+require_once("model.php");
+
+?>
