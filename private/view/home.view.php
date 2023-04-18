2592d6ded1e429723d4b56a7fe6c5a08c50bcf26
jatin-08 <yadavjatin137@gmail.com>
2023-04-19 01:00:54 +0530

    done

--- private/view/home.view.php
+++ private/view/home.view.php
@@ -5,5 +5,8 @@ $this->view("includes/navbar");
 
 ?>
 
+<div>
+    <img src="assets\image\bg-1.jpg" alt="image" class="bg-image">
+</div>
 
 <?php $this->view("includes/footer"); ?>
--- private/view/includes/navbar.view.php
+++ private/view/includes/navbar.view.php
@@ -1,6 +1,6 @@
 <!-- NavBar Section Start -->
 
-<nav class="navbar navbar-expand-lg bg-white sticky-top">
+<nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <a class="navbar-brand" href="http://localhost/LOCALJOBCONNECT/">
             <img src="assets\image\logo.svg" alt="LOCALJOBCONNECT" width="50" height="30">
@@ -12,23 +12,23 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav m-auto">
                 <li class="nav-item">
-                    <a class="nav-link openSans" aria-current="page" href="#">Home</a>
+                    <a class="nav-link poppins text-dark" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Explore</a>
+                    <a class="nav-link poppins text-dark" aria-current="page" href="#">Explore</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Become a Seller</a>
+                    <a class="nav-link poppins text-dark" aria-current="page" href="#">Become a Seller</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Contact Us</a>
+                    <a class="nav-link poppins text-dark" aria-current="page" href="#">Contact Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">About Us</a>
+                    <a class="nav-link poppins text-dark" aria-current="page" href="#">About Us</a>
                 </li>
             </ul>
-            <button class="btn btn-success mx-lg-3"><a href="#" class="text-decoration-none text-white">Sign In</a></button>
-            <button class="btn btn-success mx-lg-2"><a href="#" class="text-decoration-none text-white">Join Us</a></button>
+            <button class="btn btn-success mx-lg-3"><a href="#" class="text-decoration-none text-white poppins">Sign In</a></button>
+            <button class="btn btn-success mx-lg-2"><a href="#" class="text-decoration-none text-white poppins">Join</a></button>
         </div>
     </div>
 </nav>
