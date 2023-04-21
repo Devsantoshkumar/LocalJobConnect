1a11d56a2079d4e7847833116602a48a8a9f1523
jatin-08 <yadavjatin137@gmail.com>
2023-04-21 06:24:50 +0530

    new section completed

--- private/view/home.view.php
+++ private/view/home.view.php
@@ -36,26 +36,167 @@ $this->view("includes/navbar");
             <div class="row">
                 <h4 class="work-process-heading">How It <span style="color:var(--green-color);">Works</span></h4>
             </div>
-            <div class="row my-5">
-                <div class="offeset-md-1 col-md-3">
-                    <i class="fa-solid fa-file-invoice"></i>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique obcaecati possimus ratione
+            <div class="row mb-3">
+                <div class="offeset-md-1 col-md-3 work-process-text m-auto">
+                    <img src="assets\image\create account.png" class="w-100" alt="image">
+                    <h5 class="text-center">Create Account</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique
+                        obcaecati possimus ratione
                         accusamus doloribus provident. Reiciendis eligendi voluptate qui.</p>
                 </div>
-                <div class="offeset-md-1 col-md-3">
-                    <i class="fa-solid fa-users-viewfinder"></i>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic mollitia laudantium amet consequatur
+                <div class="offeset-md-1 col-md-3 m-auto work-process-text">
+                    <img src="assets\image\search job.png" alt="image" class="w-100">
+                    <h5 class="text-center">Search Jobs</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic mollitia
+                        laudantium amet consequatur
                         nihil voluptas voluptate incidunt, enim quaerat? At!</p>
                 </div>
-                <div class="offeset-md-1 col-md-3">
-                    <i class="fa-solid fa-file-shield"></i>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quibusdam iusto consectetur
+                <div class="offeset-md-1 col-md-3 m-auto work-process-text">
+                    <img src="assets\image\sava & appy.png" class="w-100" alt="image">
+                    <h5 class="text-center">Save & Apply</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quibusdam
+                        iusto consectetur
                         sequi expedita in quasi atque optio,
                     </p>
                 </div>
             </div>
         </div>
     </section>
+
+    <!-- Job Categories Section Start -->
+
+    <section class="containerfluid">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center fs-2 fw-bold heading-text">Browse Jobs By <span
+                            class="fw-bold fs-2 heading-text heading-color"> Categories</span></h1>
+                    <p class="text-center mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nemo!
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <a href="#" class="text-decoration-none text-end job-link">View
+                    All<i class="fa-solid fa-caret-down mx-2"></i></a>
+            </div>
+            <div class="job-category mt-2">
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+                <div class="job-category-box rounded card border-0 shadow">
+                    <div class="job-category-image rounded">
+                        <img src="assets\image\job-category-electrician.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-item-center justify-content-between">
+                            <h1 class="fw-bold fs-5 heading-text card-title">Electrician</h1>
+                            <span><i class="fa-solid fa-star fs-6 heading-color mx-1"></i>4.5 </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, accusantium.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Job Categories Section End -->
 
 
 
