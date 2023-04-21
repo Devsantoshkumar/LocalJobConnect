b1ce1a4022ed362ecbaab22c1007d7f0846d06bf
jatin-08 <yadavjatin137@gmail.com>
2023-04-22 02:41:13 +0530

    initial commit

--- private/view/includes/footer.view.php
+++ private/view/includes/footer.view.php
@@ -2,7 +2,6 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script src="<?= ASSETS ?>js/owl.carousel.min.js"></script>
 <script src="<?= ASSETS ?>js/script.js"></script>
 </body>
