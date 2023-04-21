45ff3101af9ec7361f24d69688a4adddfb89754c
Santosh Kumar <skambharti563@gmail.com>
2023-04-22 02:39:00 +0530

    done

--- private/view/includes/header.view.php
+++ private/view/includes/header.view.php
@@ -9,6 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/owl.theme.default.min.css">
     <link rel="stylesheet" href="<?= ASSETS ?>css/colors.css">
     <link rel="stylesheet" href="<?= ASSETS ?>css/main.css">
     <link rel="stylesheet" href="<?= ASSETS ?>css/style.css">
