922fae34ac2de7843689e5cc57f1a49048d9d4a4
jatin-08 <yadavjatin137@gmail.com>
2023-05-20 03:31:21 +0530

    In work

--- private/view/profile.view.php
+++ private/view/profile.view.php
@@ -16,24 +16,78 @@ $this->view("profileNav");
 
 
 
-<<<<<<< HEAD
-<section class="container-fluid my-5">
-    <div class="profile-section card border-0 p-3 bg-white shadow">
-        <div class="profile-card">
-            <div class="profile-img rounded-circle">
-                <img src="assets\image\employee (4).jpg" class="image-fluid" alt="image">
-=======
+
 <section class="container-fluid my-4">
     <div class="row">
-        <div class="col-3">
-            <div class="card border-0 p-4 shadow-sm">
-                Profile Id
->>>>>>> main
+        <div class="col-3 card border-0 p-4 shadow-sm">
+            <div class="d-flex flex-row align-items-center gap-4 p-4">
+                <div class="profile-img">
+                    <img src="assets\image\employee (1).jpg" alt="image">
+                </div>
+                <span>
+                    <a href="#" class="text-decoration-none">
+                        <?php
+
+                        if (Auth::loggedIn()) {
+                            echo Auth::user('firstname');
+                        } else {
+                            echo "No Name";
+                        }
+                        ?>
+                    </a>
+                </span>
             </div>
+            <hr>
+            <ul class="profile-list list-unstyled">
+                <li>
+                    <a href="#">Inbox Response Rate</a>
+
+                    <a href="#">100%</a>
+                </li>
+                <li>
+                    <a href="#">Inbox Response Time</a>
+                    <a href="#">100%</a>
+                </li>
+                <li>
+                    <a href="#">Order Response Rate</a>
+                    <a href="#">100%</a>
+                </li>
+                <li>
+                    <a href="#">Delivered On Time</a>
+                    <a href="#">100%</a>
+                </li>
+                <li>
+                    <a href="#">Order Completion</a>
+                    <a href="#">100%</a>
+                </li>
+                <hr>
+                <li>
+                    <a href="#">Total Earning</a>
+                    <a href="#">US $0</a>
+                </li>
+            </ul>
         </div>
         <div class="col">
-            <div class="card border-0 p-4 shadow-sm">
-                profile card
+            <div class="d-flex flex-row justify-content-between align-items-center card border-0 p-4 shadow-sm">
+                <h1 class="fs-5 profile-content-head">Active Orders - 0($0)</h1>
+                <select class="form-select w-25" aria-label="Default select example">
+                    <option selected>Active Orders -0($0)</option>
+                    <option value="1">Completed</option>
+                    <option value="2">Canceled</option>
+                    <option value="3">Pending</option>
+                </select>
+            </div>
+            <h1 class="fs-6 profile-content-head mt-5 mb-1 p-3">Upgrade Your Work
+                <hr>
+            </h1>
+            <div class="d-flex flex-row justify-content-between align-items-center card border-0 p-4 shadow-sm">
+                <h1 class="fs-5 profile-content-head">Active Orders - 0($0)</h1>
+                <select class="form-select w-25" aria-label="Default select example">
+                    <option selected>Active Orders -0($0)</option>
+                    <option value="1">Completed</option>
+                    <option value="2">Canceled</option>
+                    <option value="3">Pending</option>
+                </select>
             </div>
         </div>
     </div>
