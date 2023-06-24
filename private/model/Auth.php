130e68845aab1b42f310d0fa9d66d8f5ed58225a
Jatin Yadav <yadavjatin137@gmail.com>
2023-06-25 00:24:28 +0530

    error solved

--- private/model/Auth.php
+++ private/model/Auth.php
@@ -11,15 +11,15 @@ class Auth
     // Logout() -> It will unset all the value that is store by the session variable
     public static function logout()
     {
-        if (isset($_SESSION['USER'])) {
-            unset($_SESSION['USER']);
+        if (isset($_SESSION['USERS'])) {
+            unset($_SESSION['USERS']);
         }
     }
 
     // loggedIn() -> It wil if any user is logged or not.
     public static function loggedIn()
     {
-        if (isset($_SESSION['USER'])) {
+        if (isset($_SESSION['USERS'])) {
             return true;
         }
         return false;
@@ -28,8 +28,8 @@ class Auth
     // user() -> It help to display the thing you want to display when you loggedIn.
     public static function user($user)
     {
-        if (isset($_SESSION['USER'])) {
-            return $_SESSION['USER']->$user;
+        if (isset($_SESSION['USERS'])) {
+            return $_SESSION['USERS']->$user;
         }
         return false;
     }
