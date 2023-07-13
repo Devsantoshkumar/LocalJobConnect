0a64486d77631500587afb1bfa9c8ba2ba54ab42
jatin-08 <yadavjatin137@gmail.com>
2023-07-14 00:57:35 +0530

    done

--- private/controller/Employees.php
+++ private/controller/Employees.php
@@ -15,14 +15,20 @@ class Employees extends Controller
         $this->view("employees", ['errors' => $errors, 'userData' => $userdata]);
     }
 
-    function searching()
-    {
-        $errors = [];
-        $rows = [];
-        $search = '';
+    // function searching()
+    // {
+    //     $errors = [];
+    //     $rows = [];
+    //     $search = '';
 
-        $user = new User();
+    //     $user = new User();
 
-        
-    }
+    //     if (isset($_GET['search'])) {
+    //         $search = $_GET['search'];
+    //         $rows = $user->query("SELECT * FROM users LEFT JOIN categorys ON users.job_category_id = categorys.categorys_id WHERE category_name LIKE '%$search%'");
+    //         show($rows);
+    //     }
+
+    //     $this->view('employees', ['errors' => $errors, 'rows' => $rows, 'search' => $search]);
+    // }
 }
